28f004123857bbee43fe3f50153a114c23684685
Frank Purcell <fxpurcell@gmail.com>
2018-03-08 22:01:47 -0800

    some work on database utility object

---
+++ ott/boundary/control/loader.py
@@ -0,0 +1,37 @@
+from ott.utils.parse.cmdline import db_cmdline
+
+import logging
+logging.basicConfig()
+log = logging.getLogger(__file__)
+
+
+def make_session(url, schema, is_geospatial=False, create_db=False):
+    return Database.make_session(url, schema, is_geospatial, create_db)
+
+def load_data(session, agency_id, trips_url, alerts_url, vehicles_url):
+    ret_val = True
+    return ret_val
+
+def parse(session, agency_id, feed_url, clear_first=False):
+    ret_val = True
+    return ret_val
+
+
+def main():
+    cmdline = db_cmdline.db_parser()
+    args = cmdline.parse_args()
+    print args
+
+    session = Database.make_session(args.database_url, args.schema, args.geo, args.create)
+
+    url = 'http://trimet.org/transweb/ws/V1/FeedSpecAlerts/appId/3819A6A38C72223198B560DF0/includeFuture/true'
+    url = 'http://trimet.org/transweb/ws/V1/TripUpdate/appId/3819A6A38C72223198B560DF0/includeFuture/true'
+    #url = 'http://developer.trimet.org/ws/gtfs/VehiclePositions/appId/3819A6A38C72223198B560DF0'
+    if args.url and len(args.url) > 1:
+        url = args.url
+    parse(session, args.agency, url, args.clear)
+
+
+if __name__ == '__main__':
+    main()
+
