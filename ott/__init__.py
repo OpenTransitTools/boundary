87331378cf2cfd85db750af20ffdeed88093f71f
Frank Purcell <fxpurcell@gmail.com>
2018-03-12 13:52:54 -0700

    bit more loading of bounds

--- ott/__init__.py
+++ ott/__init__.py
@@ -1,1 +1,4 @@
 __import__('pkg_resources').declare_namespace(__name__)
+
+from ott.boundary.model.ada import Ada # noqa
+from ott.boundary.model.district import District # noqa
--- ott/boundary/control/loader.py
+++ ott/boundary/control/loader.py
@@ -1,29 +1,17 @@
 from ott.utils.parse.cmdline import db_cmdline
+from ott.boundary.control.database import Database
 
 import logging
-logging.basicConfig()
 log = logging.getLogger(__file__)
-
-
-def make_session(url, schema, is_geospatial=False, create_db=False):
-    return Database.make_session(url, schema, is_geospatial, create_db)
-
-
-def load_data(session, agency_id, trips_url, alerts_url, vehicles_url):
-    ret_val = True
-    return ret_val
-
-
-def parse(session, agency_id, feed_url, clear_first=False):
-    ret_val = True
-    return ret_val
 
 
 def load_db():
     cmdline = db_cmdline.db_parser()
     args = cmdline.parse_args()
-    print args
+    print Database.get_base_subclasses()
 
+
+def XXload_db():
     session = Database.make_session(args.database_url, args.schema, args.geo, args.create)
 
     url = 'http://trimet.org/transweb/ws/V1/FeedSpecAlerts/appId/3819A6A38C72223198B560DF0/includeFuture/true'
@@ -36,6 +24,7 @@ def load_db():
 
 def main():
     load_db()
+
 
 if __name__ == '__main__':
     main()
