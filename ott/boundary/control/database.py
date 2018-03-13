ca11791ba37498ffd03076992ab6bd14b1dd3096
Frank Purcell <fxpurcell@gmail.com>
2018-03-12 17:04:32 -0700

    refactor the Database() config to allow extending via inheritance

--- ott/boundary/control/database.py
+++ ott/boundary/control/database.py
@@ -5,22 +5,11 @@ class Database(GtfsdbDatabase):
 
     def __init__(self, **kwargs):
         super(Database, self).__init__(**kwargs)
+        self.sorted_class_names.append('District')
+        self.sorted_class_names.append('Ada')
 
     @classmethod
     def get_base_subclasses(cls):
         from ott.boundary.model.base import Base
         return Base.__subclasses__()
-
-    @classmethod
-    def factory_from_cmdline(cls, args):
-        # import pdb; pdb.set_trace()
-        kwargs = dict(
-            is_geospatial=args.is_geospatial,
-            schema=args.schema,
-            url=args.database_url,
-        )
-        db = Database(**kwargs)
-        if args.create:
-            db.create()
-        return db
 
